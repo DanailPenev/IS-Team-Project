244d3a2295b90b0f5769f8b88e5c72ea10d886de
Danail Penev <2181508p@student.gla.ac.uk>
2017-11-24 15:46:15 +0000

    Demo

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 var mode = "<h1 class=\"cover-heading\">Select a mode</h1>\
             <p class=\"lead\">\
               <button onclick=\"newMap()\" class=\"btn btn-lg btn-secondary\">Map</button>\
-              <button onclick=\"stats()\" class=\"btn btn-lg btn-secondary\">Stats</button>\
+              <button onclick=\"parse()\" class=\"btn btn-lg btn-secondary\">Stats</button>\
             </p>";
 
 var xmlOpened;
@@ -71,5 +71,98 @@ function openFile() {
     };
     reader.readAsText(file);
   }
-  
+};
+
+function parse(){
+  $("#removeme").css("visibility", "hidden");
+  var element = document.getElementById("removeme");
+    element.outerHTML = "";
+    delete element;
+  $("#chart").css("width", "100%");
+  $("#chart").css("height", "40%");
+  if (window.DOMParser){
+      parser = new DOMParser();
+      xmlDoc = parser.parseFromString(window.xmlOpened, "text/xml");
+  }
+  else // Internet Explorer
+  {
+      xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+      xmlDoc.async = false;
+      xmlDoc.loadXML(txt);
+  }
+  var time = xmlDoc.getElementsByTagName("time");
+  var times = []
+  for (i=0; i<time.length; i++){
+      times.push(time[i].innerHTML.split('T')[1].split('Z')[0].split('.')[0]);
+  }
+  times.shift();
+  var elements = xmlDoc.getElementsByTagName("ele");
+  var elevations = [];
+  for (i=0; i<elements.length; i++){
+      elevations.push(elements[i].innerHTML);
+  }
+  console.log(elevations);
+  console.log(times);
+  var points = [];
+  for (i=0; i<times.length; i++){
+    var point = new Object();
+    point.x = times[i];
+    point.y = elevations[i];
+    points.push(point);
+  }
+  console.log(points);
+  var ctx = document.getElementById("myChart");
+  var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: times,
+      datasets: [
+      {
+        label: "Elevation over time",
+        data: elevations,
+        backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            fontColor: "white",
+      }
+      ]
+    },
+    options: {
+      title: {
+            display: true,
+            text: 'Relationship between elevation and time for the file that was entered',
+            fontColor: "white",
+            fontSize: "25"
+        },
+      scales: {
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Elevation in meters',
+            fontColor: "white",
+      }
+      }],
+      xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Local time',
+            fontColor: "white",
+      }
+      }]
+  }
+    }
+  });
 };
